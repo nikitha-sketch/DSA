d9e455818ea7bf54604e579894d66d041d75bc41
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-03-04 11:16:55 +0530

    Time: 1917 ms (5.07%) | Memory: 45.5 MB (23.62%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.java
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -1,6 +1,6 @@
 class Solution {
     public int totalHammingDistance(int[] nums) {
-       int total=0;
+       /*int total=0;
        int n=nums.length;
        for(int bit=0;bit<32;bit++){
             int countOnes=0;
@@ -10,6 +10,14 @@ class Solution {
             int countZeroes=n-countOnes;
             total+=countOnes*countZeroes;
        }
-       return total;
+       return total;*/
+       int n=nums.length;
+       int sum=0;
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            sum+=Integer.bitCount(nums[i]^nums[j]);
+        }
+       }
+       return sum;
     }
 }
