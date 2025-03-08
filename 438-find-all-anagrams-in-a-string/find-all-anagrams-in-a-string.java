0894e04cfbc6165aeb1a00be1a6cc9ac5c538291
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-03-08 10:40:20 +0530

    Time: 9 ms (89.35%) | Memory: 44.9 MB (60.47%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer>result=new ArrayList<>();
+       /* List<Integer>result=new ArrayList<>();
         if(s.length()<p.length())return result;
 
         Map<Character,Integer>pMap=new HashMap<>();
@@ -32,6 +32,27 @@ class Solution {
             }
             right++;
         }
+        return result;*/
+        int ls=s.length(),lp=p.length();
+        List<Integer>result=new ArrayList<>();
+        if(lp>ls)return result;
+        int freq[]=new int[26];
+        for(int i=0;i<lp;i++){
+            freq[p.charAt(i)-'a']--;
+            freq[s.charAt(i)-'a']++;
+        }
+        if(allZero(freq))result.add(0);
+        for(int i=lp;i<ls;i++){
+            freq[s.charAt(i)-'a']++;
+            freq[s.charAt(i-lp)-'a']--;
+            if(allZero(freq))result.add(i-lp+1);
+        }
         return result;
+    }
+    public boolean allZero(int freq[]){
+        for(int i:freq){
+            if(i!=0)return false;
+        }
+        return true;
     }
 }
