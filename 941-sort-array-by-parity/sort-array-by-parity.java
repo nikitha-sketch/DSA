d4d3b12ea028c50c1eef78ebd4d655f0f04d5e9b
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-03-18 21:39:58 +0530

    Time: 8 ms (5.31%) | Memory: 45.5 MB (8.47%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        ArrayList<Integer>evenList=new ArrayList<>();
+       /* ArrayList<Integer>evenList=new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             if(nums[i]%2==0){
                 evenList.add(nums[i]);
@@ -14,6 +14,16 @@ class Solution {
         }
         evenList.addAll(oddList);
         //return evenList;
-        return evenList.stream().mapToInt(Integer::intValue).toArray();
+        return evenList.stream().mapToInt(Integer::intValue).toArray();*/
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int num:nums){
+            if(num%2==0){
+                list.add(0,num);
+            }
+            else{
+                list.add(num);
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
