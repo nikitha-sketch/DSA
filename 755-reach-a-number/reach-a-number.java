2a561111b2d7e404db7949fc65b75221674e4701
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-04-18 18:00:37 +0530

    Time: 2 ms (48.25%) | Memory: 40.7 MB (31.55%) - LeetSync

---
+++ 755-reach-a-number/reach-a-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reachNumber(int target) {
+        target=Math.abs(target);
+        int sum=0,steps=0;
+        while(sum<target || (sum-target)%2!=0){
+            steps++;
+            sum+=steps;
+        }
+        return steps;
+    }
+}
