b4969595fea66fcdc534f85c4aa8340e303764fe
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-07-16 12:28:22 +0530

    Time: 1 ms (46.42%) | Memory: 44.8 MB (84.11%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void dfs(int row,int col,int image[][],int ans[][],int color,int inicolor,int delRow[],int delCol[]){
+    /*public void dfs(int row,int col,int image[][],int ans[][],int color,int inicolor,int delRow[],int delCol[]){
         ans[row][col]=color;
         int n=image.length;
         int m=image[0].length;
@@ -18,5 +18,30 @@ class Solution {
         int delCol[]={0,1,0,-1};
         dfs(sr,sc,image,ans,color,inicolor,delRow,delCol);
         return ans;
+    }*/
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int originalColor=image[sr][sc];
+        if(originalColor==color)return image;
+         
+         int rows=image.length;
+         int cols=image[0].length;
+          Queue<int[]>queue=new LinkedList<>();
+          queue.add(new int[]{sr,sc});
+          image[sr][sc]=color;
+          int directions[][]={{0,1},{1,0},{0,-1},{-1,0}};
+          while(!queue.isEmpty()){
+            int cell[]=queue.poll();
+            int r=cell[0],c=cell[1];
+            for(int dir[]:directions){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+                if(nr>=0 && nr<rows && nc>=0 && nc<cols && image[nr][nc]==originalColor){
+                    queue.add(new int[]{nr,nc});
+                    image[nr][nc]=color;
+                }
+            }
+          }
+          return image;
     }
 }
