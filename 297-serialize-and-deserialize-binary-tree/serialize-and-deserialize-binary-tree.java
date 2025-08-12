e46f8ccc27d848288675979bc03e463b901b20d4
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-08-12 15:46:46 +0530

    Time: 18 ms (36.48%) | Memory: 46.5 MB (21.26%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -10,7 +10,7 @@
 public class Codec {
 
     // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
+   /* public String serialize(TreeNode root) {
         if(root==null){
             return "";
         }
@@ -51,6 +51,24 @@ public class Codec {
                 q.add(right);
             }
         }
+        return root;
+    }*/
+    public String serialize(TreeNode root) {
+        if(root==null)return "#";
+        return root.val+","+serialize(root.left)+","+serialize(root.right);
+    }
+    public TreeNode deserialize(String data) {
+        Queue<String>queue=new LinkedList<>(Arrays.asList(data.split(",")));
+        return helper(queue);
+    }
+    public TreeNode helper(Queue<String>queue){
+        String s=queue.poll();
+        if(s.equals("#")){
+            return null;
+        }
+        TreeNode root=new TreeNode(Integer.valueOf(s));
+        root.left=helper(queue);
+        root.right=helper(queue);
         return root;
     }
 }
