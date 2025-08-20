abfda194d8457cfbfe09ae7142182bf209253719
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-08-20 22:39:36 +0530

    Time: 2 ms (72.24%) | Memory: 41.7 MB (52.92%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
-import java.util.Collections;
 class Solution {
     public int hIndex(int[] citations) {
        Arrays.sort(citations);
        int n=citations.length;
        for(int i=0;i<n;i++){
-        int h=n-i;
-        if(citations[i]>=h){
-            return h;
-        }
+            int h=n-i;
+            if(citations[i]>=h){
+                return h;
+            }
        }
        return 0;
     }
