3b0447281401cf427f3e524da311ca56b56af173
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-08-24 09:27:52 +0530

    Time: 1 ms (99.86%) | Memory: 47.2 MB (7.53%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -28,7 +28,7 @@ class Solution {
         }
         return list1;*/
 
-        ListNode temp1=list1;
+        /*ListNode temp1=list1;
         while(a!=1){
             temp1=temp1.next;
             a--;
@@ -43,6 +43,24 @@ class Solution {
             list2=list2.next;
         }
         list2.next=temp2;
+        return list1;*/
+
+        ListNode temp=list1;
+        ListNode cur=list1;
+        for(int i=0;i<=b;i++){
+            cur=cur.next;
+        }
+
+        for(int i=0;i<a-1;i++){
+            temp=temp.next;
+        }
+
+        temp.next=list2;
+        ListNode tail=list2;
+        while(tail.next!=null){
+            tail=tail.next;
+        }
+        tail.next=cur;
         return list1;
     }
 }
