b5f574c9eaed690a8e3f7bdc4ca6afe5fe96ac4e
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-08-25 06:15:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (18.70%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -15,13 +15,28 @@ class Solution {
         }
         return ans;*/
 
-        if(nums[nums.length-1]<target)return nums.length;
+       /* if(nums[nums.length-1]<target)return nums.length;
         for(int i=0;i<nums.length;i++){
             if(target==nums[i])return i;
         }
         for(int i=1;i<nums.length;i++){
             if(nums[i-1]<target && nums[i]>target)return i;
         }
-        return 0;
+        return 0;*/
+
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
     }
 }
