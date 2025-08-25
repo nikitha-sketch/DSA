b26cac7546b33fd6c4418029d0531267ccf76375
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-08-25 06:03:56 +0530

    Time: 3 ms (37.77%) | Memory: 41.9 MB (47.84%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValid(String s) {
+        /*Stack<Character>stack=new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='(')
+                stack.push(')');
+            else if(c=='[')
+                stack.push(']');
+            else if(c=='{')
+                stack.push('}');
+            else if(stack.isEmpty()||stack.pop()!=c)
+                return false;
+        }
+        return stack.isEmpty();*/
+
+        Stack<Character>stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
+            else if(!stack.isEmpty() && ch==')' && stack.peek()=='('){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && ch==']' && stack.peek()=='['){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && ch=='}' && stack.peek()=='{'){
+                stack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
