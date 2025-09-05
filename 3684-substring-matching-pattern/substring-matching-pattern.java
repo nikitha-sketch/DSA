8d9d883d72049b0475b29605ae78e65df37c5383
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-09-05 19:12:55 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (95.43%) - LeetSync

--- 3684-substring-matching-pattern/substring-matching-pattern.java
+++ 3684-substring-matching-pattern/substring-matching-pattern.java
@@ -1,8 +1,23 @@
 import java.util.regex.Pattern;
 class Solution {
     public boolean hasMatch(String s, String p) {
-        String parts[]=p.split("\\*",-1);
+        /*String parts[]=p.split("\\*",-1);
         String regex=Pattern.quote(parts[0])+".*"+Pattern.quote(parts[1]);
-        return s.matches(".*"+regex+".*");
+        return s.matches(".*"+regex+".*");*/
+        int startIndex=p.indexOf("*");
+        String prefix=p.substring(0,startIndex);
+        String suffix=p.substring(startIndex+1);
+
+        if(prefix.isEmpty() && suffix.isEmpty())return true;
+
+        for(int i=0;i+prefix.length()<=s.length();i++){
+            if(s.startsWith(prefix,i)){
+                int j=i+prefix.length();
+                if(s.indexOf(suffix,j)!=-1){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
