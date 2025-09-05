95171ba967703524a0cf94f1ca93dc17c9820a97
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-09-05 19:25:55 +0530

    Time: 1 ms (99.57%) | Memory: 43.4 MB (66.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,10 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
+       /* if(s.length()!=t.length())return false;
         char sArray[]=s.toCharArray();
         char tArray[]=t.toCharArray();
         Arrays.sort(sArray);
         Arrays.sort(tArray);
-        return Arrays.equals(sArray,tArray);
+        return Arrays.equals(sArray,tArray);*/
+        if(s.length()!=t.length())return false;
+        int freq[]=new int[256];
+
+        for(char ch:s.toCharArray()){
+            freq[ch]++;
+        }
+
+        for(char ch:t.toCharArray()){
+            freq[ch]--;
+            if(freq[ch]<0)return false;
+        }
+
+        for(int cnt:freq){
+            if(cnt!=0)return false;
+        }
+
+        return true;
     }
 }
