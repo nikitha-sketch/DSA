939fbc1b3359b6650afb5f5bd19d8c413997d84c
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-09-15 18:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.65%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,12 +1,18 @@
 class Solution {
     public int kthGrammar(int n, int k) {
-        if(n==0)return 1;
+        /*if(n==0)return 1;
         int half=1<<(n-2);
         if(k<=half){
             return kthGrammar(n-1,k);
         }
         else{
             return 1-kthGrammar(n-1,k-half);
-        }
+        }*/
+
+        if(n==1 || k==1)return 0;
+        int size=(int)Math.pow(2,n-1);
+        int mid=size/2;
+        if(k>mid)return 1-kthGrammar(n-1,k-mid);
+        else return kthGrammar(n-1,k);
     }
 }
