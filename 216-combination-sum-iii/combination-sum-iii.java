6b1260d39edba152d177abb0596908641866078e
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-09-20 22:27:09 +0530

    Time: 3 ms (2.72%) | Memory: 41.8 MB (6.12%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -3,21 +3,24 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), k, n, 1);
+
+        // There are 2^9 subsets of {1..9}, i.e., 512 possibilities
+        for (int mask = 0; mask < (1 << 9); mask++) {
+            List<Integer> list = new ArrayList<>();
+            int sum = 0, cnt = 0;
+
+            for (int i = 0; i < 9; i++) {
+                if ((mask & (1 << i)) != 0) {  // pick number (i+1)
+                    list.add(i + 1);
+                    sum += (i + 1);
+                    cnt++;
+                }
+            }
+
+            if (cnt == k && sum == n) {
+                result.add(list);
+            }
+        }
         return result;
-    }
-
-    private void backtrack(List<List<Integer>> result, List<Integer> temp, int k, int remain, int start) {
-        if (temp.size() == k && remain == 0) {
-            result.add(new ArrayList<>(temp));
-            return;
-        }
-        if (temp.size() >= k || remain < 0) return;
-
-        for (int i = start; i <= 9; i++) {
-            temp.add(i);
-            backtrack(result, temp, k, remain - i, i + 1);
-            temp.remove(temp.size() - 1);
-        }
     }
 }
