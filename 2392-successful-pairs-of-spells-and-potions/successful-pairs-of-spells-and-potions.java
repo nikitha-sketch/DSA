ca95df1777ed14b57814551aa2b49a9b85c29819
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-10-08 09:34:52 +0530

    Time: 48 ms (35.31%) | Memory: 65 MB (55.55%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,51 @@
+//this passes only 51/57 testcase this is brute force method
+/*class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n=spells.length;
+        int m=potions.length;
+        int ans[]=new int[n];
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                long prod=(long)spells[i]*potions[j];
+                if(prod>=success){
+                    cnt++;
+                }
+            }
+            ans[i]=cnt;
+            cnt=0;
+        }
+        return ans;
+    }
+}*/
+
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n=spells.length;
+        int m=potions.length;
+        int ans[]=new int[n];
+        Arrays.sort(potions);
+
+        for(int i=0;i<n;i++){
+            long spell=spells[i];
+            long minPotion=(success+spell-1)/spell;
+            int idx=lowerBound(potions,minPotion);
+            ans[i]=m-idx;
+        }
+        return ans;
+    }
+    public int lowerBound(int arr[],long target){
+        int left=0,right=arr.length;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+}
