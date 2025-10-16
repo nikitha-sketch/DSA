f463d75d6c3749ae5e93ef3374451f9bc5b2f9dd
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-10-16 09:37:41 +0530

    Time: 8 ms (51.27%) | Memory: 58 MB (72.59%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findSmallestInteger(int[] nums, int value) {
-        Map<Integer,Integer>map=new HashMap<>();
+        /*Map<Integer,Integer>map=new HashMap<>();
         for(int num:nums){
             int mod=((num%value)+value)%value;
             map.put(mod,map.getOrDefault(mod,0)+1);
@@ -14,7 +14,26 @@ class Solution {
             }
             map.put(mod,map.get(mod)-1);
             ans++;
+        }*/
+
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            nums[i] = ((nums[i] % value) + value) % value;
         }
-       // return ans;
+
+        int freq[]=new int[value];
+        for(int num:nums){
+            freq[num%value]++;
+        }
+
+        int ans=0;
+        while(true){
+            int mod=ans%value;
+            if(freq[mod]==0){
+                return ans;
+            }
+            freq[mod]--;
+            ans++;
+        }
     }
 }
