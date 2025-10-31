0bd23c05f91b82e0395433e4a57e6f7fcaabfb5d
Nikitha Reddy <nikithareddy1103@gmail.com>
2025-10-31 09:07:29 +0530

    Time: 2 ms (63.09%) | Memory: 44.5 MB (94.02%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,13 +1,5 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-         Map<Integer, Long> map = Arrays.stream(nums)
-            .boxed()
-            .collect(Collectors.groupingBy(num -> num, Collectors.counting()));
-
-        return map.entrySet().stream()
-            .filter(entry -> entry.getValue() >= 2)
-            .mapToInt(Map.Entry::getKey)
-            .toArray();
         /*Map<Integer,Integer>map=new HashMap<>();
         List<Integer>list=new ArrayList<>();
         int n=nums.length;
@@ -25,20 +17,23 @@ class Solution {
          }
         return arr;
         */
-        /*List<Integer>list=new ArrayList<>();
+        List<Integer>list=new ArrayList<>();
         int n=nums.length;
-        int arr[]=new int[n];
-        int cnt=0;
         for(int i=0;i<n;i++){
+            int cnt=1;
             for(int j=i+1;j<n;j++){
                 if(nums[i]==nums[j]){
                     cnt++;
                 }
-                if(cnt>=2)list.add(nums[i]);
             }
-            
+            if(cnt>=2 && !list.contains(nums[i])){
+                list.add(nums[i]);
+            }
         }
-        return arr;*/
-        
+        int arr[]=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
     }
 }
