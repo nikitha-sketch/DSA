94fc139c5e723233b6a009eac7fd3eea85f49673
Nikitha Reddy <nikithareddy1103@gmail.com>
2026-05-31 19:20:53 +0530

    Time: 26 ms (81.52%) | Memory: 107.4 MB (47.81%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long curMass=mass;
+        for(int asteroid:asteroids){
+            if(curMass<asteroid){
+                return false;
+            }
+            curMass+=asteroid;
+        }
+        return true;
+    }
+}
