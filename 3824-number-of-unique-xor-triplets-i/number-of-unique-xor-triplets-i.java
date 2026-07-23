c60fa71aee8ebd7d4b0db0ae7ea5fc6f83b599e8
Nikitha Reddy <nikithareddy1103@gmail.com>
2026-07-23 18:23:12 +0530

    Time: 0 ms (100.00%) | Memory: 125.7 MB (10.20%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        /*int n=nums.length;
+        int xor=0;
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                for(int k=j;k<n;k++){
+                    xor=nums[i]^nums[j]^nums[k];
+                    set.add(xor);
+                }
+            }
+        }
+        return set.size();*/
+        int n=nums.length;
+        if(n<=2){
+            return n;
+        }
+        int result=32-32 - Integer.numberOfLeadingZeros(n);;
+        return 1<<result;
+    }
+}
